5f8ddbe35644657f4db82b78543ad6585a24b72f
adielx2890x <adielx2890x@gmail.com>
2023-08-07 14:45:00 -0600

    deay

---
+++ src/keyboard.js
@@ -0,0 +1,11 @@
+class Keyboard{
+    constructor(){
+        this.keys = {}
+    }
+    drawKeyboard(){
+        this.keys.map(key =>{
+          
+        })
+          console.log('[]')
+    }
+}
---
+++ src/result.js
@@ -0,0 +1,7 @@
+class Result {
+  constructor() {
+    this.menssage = "Volver a Empezar";
+  }
+
+  showResult(valor) {}
+}
