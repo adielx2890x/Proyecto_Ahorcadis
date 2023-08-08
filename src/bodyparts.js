9f3b1985c5f7f3454068f05524d4f10ecfaa8ec4
adielx2890x <adielx2890x@gmail.com>
2023-08-08 11:35:58 -0600

    un pedazo de cod

---
+++ src/bodyparts.js
@@ -0,0 +1,7 @@
+/*
+Crea en la carpeta src una Clase BodyParts.
+
+Esta clase deberá tener como propiedad un arreglo de imagenes(Las imagenes que ya agregaste en uno de los tiquetes anteriores).
+
+Además deberá tener una función encargada de actualizar la imagen según los intentos que vayan quedando.
+ */
