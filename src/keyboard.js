a71760cb2c5247e1cecd4ba77850ae89f8a0fb82
adielx2890x <adielx2890x@gmail.com>
2023-08-08 15:40:32 -0600

    ultima de today

--- src/keyboard.js
+++ src/keyboard.js
@@ -27,11 +27,12 @@ class Keyboard {
       [
       "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ", "Z", "X", "C", "V", "B", "N", "M" 
     ]
-  
   }
   drawKeyboard() {
     this.keys.map((key) => {});
     console.log(this.keys);
+
+    return this.drawKeyboard
   }
 }
 console.log(Keyboard)
